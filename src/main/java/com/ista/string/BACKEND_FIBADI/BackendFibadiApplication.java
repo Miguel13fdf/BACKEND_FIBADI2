27fbe033e8cae3212dee914155f83d54ff13547c
Miguel13fdf <122501535+Miguel13fdf@users.noreply.github.com>
2023-07-06 16:32:07 -0500

    Initial commit

---
+++ src/main/java/com/ista/string/BACKEND_FIBADI/BackendFibadiApplication.java
@@ -0,0 +1,13 @@
+package com.ista.string.BACKEND_FIBADI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFibadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFibadiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ista/string/BACKEND_FIBADI/BackendFibadiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.string.BACKEND_FIBADI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFibadiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
