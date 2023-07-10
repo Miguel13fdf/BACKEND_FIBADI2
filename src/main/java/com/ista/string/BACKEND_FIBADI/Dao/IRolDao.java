53199e00971af80de8f54ee98b9d1e945cabfd7a
Santiago G <santiagogcan@gmail.com>
2023-07-10 17:03:57 -0500

    Rol

---
+++ src/main/java/com/ista/string/BACKEND_FIBADI/Dao/IRolDao.java
@@ -0,0 +1,9 @@
+package com.ista.string.BACKEND_FIBADI.Dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ista.string.BACKEND_FIBADI.Model.Rol;
+
+public interface IRolDao extends CrudRepository<Rol, Long> {
+
+}
