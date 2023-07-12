8addbd1a992fc98392367b9a3ff004dbff023386
Miguel13fdf <122501535+Miguel13fdf@users.noreply.github.com>
2023-07-11 19:52:03 -0500

    Login2

--- src/main/java/com/ista/string/BACKEND_FIBADI/Restcontroller/UsuarioRestController.java
+++ src/main/java/com/ista/string/BACKEND_FIBADI/Restcontroller/UsuarioRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.ista.string.BACKEND_FIBADI.Model.Usuario;
 import com.ista.string.BACKEND_FIBADI.Model.Services.IUsuarioService;
-
+@CrossOrigin(origins = {"http://localhost:8080"})
 @RestController
 @RequestMapping ("/tecazuay")
 public class UsuarioRestController {
@@ -54,13 +55,13 @@ public class UsuarioRestController {
 	public Usuario findUserByCriterio(@RequestParam("criterio") String criterio) {
 	    return usuarioService.findUsuarioByCriterio(criterio);
 	}
-	@GetMapping("login/{usuario}/{contrasenia}")
-	public String login(@PathVariable("usuario")String usuario,@PathVariable("contrasenia")String contrasenia) {
+	@PostMapping("/login/{usuario}/{contrasenia}")
+	public Usuario login(@RequestBody Usuario usuario) {
 		String login="ERROR";
-		List<Usuario> usuario1=usuarioService.findByUsuarioAndContrasenia(usuario, contrasenia);
-		if(!usuario1.isEmpty()) {
-			login="Ok";
+		List<Usuario> usuarios=usuarioService.findByUsuarioAndContrasenia(usuario.getUsuario(), usuario.getContrasenia());
+		if(!usuarios.isEmpty()) {
+			return usuarios.get(0);
 		}
-		return login;
+		return null;
 	}
 }
