169c918c662da5525cd1624351594dd84e3a4416
LeninC <cordovalenin.44@gmail.com>
2023-07-15 23:24:02 -0500

    New Changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -10,13 +10,13 @@ INSERT INTO PERSONA (per_cod, per_apellidomater, per_apellidopater, per_cedula, 
 INSERT INTO PERSONA (per_cod, per_apellidomater, per_apellidopater, per_cedula, per_email, per_fechanac, per_genero, per_primernom, per_segundonom,per_telefono)VALUES(920, 'BUESTAN', 'NARVAEZ','0103937561', 'freddy.narvaez@tecazuay.edu.ec','1985-11-07', 'M', 'FREDDY', 'EDUARDO', '0998343465');
 INSERT INTO PERSONA (per_cod, per_apellidomater, per_apellidopater, per_cedula, per_email, per_fechanac, per_genero, per_primernom, per_segundonom,per_telefono)VALUES(915, 'URGILÉS', 'ORTIZ','0301162988', 'claudio.ortiz@tecazuay.edu.ec','1970-02-04', 'M', 'CLAUDIO', 'FERNANDO', '0988818368');
 INSERT INTO PERSONA (per_cod, per_apellidomater, per_apellidopater, per_cedula, per_email, per_fechanac, per_genero, per_primernom, per_segundonom,per_telefono)VALUES(1749, 'MARIÑO', 'ELHABIL','0105704597', 'catherine.elhabil@tecazuay.edu.ec','1987-12-08', 'F', 'CATHERINE', 'CUMANDA', '0979195915');
+INSERT INTO PERSONA (per_cod, per_apellidomater, per_apellidopater, per_cedula, per_email, per_fechanac, per_genero, per_primernom, per_segundonom,per_telefono)VALUES(1, 'DIAZ', 'HARO','0107186900', 'miguel.haro@tecazuay.edu.ec','2003-11-30', 'M', 'MIGUEL', 'ALEJANDRO', '0998501608');
 INSERT INTO ROLES (rol_estado, rol_nombre) values (true, 'Super Usuario');
 INSERT INTO ROLES (rol_estado, rol_nombre) values (true, 'Custodio');
-INSERT INTO USUARIOS (usu_per_cod, contrasenia, usu_estado, usuario) values (902, 'admin', true, 'admin');
+INSERT INTO USUARIOS (usu_per_cod, contrasenia, usu_estado, usuario) values (1, 'admin', true, 'admin');
 INSERT INTO USUARIOS (usu_per_cod, contrasenia, usu_estado, usuario) values (866, 'custodio1', true, 'gabriela.aguirre@tecazuay.edu.ec');
 INSERT INTO USUARIOS_ROLES (usuario_id, rol_id) values (1, 1);
 INSERT INTO USUARIOS_ROLES (usuario_id, rol_id) values (2, 2);
 INSERT INTO CATEGORIA(cat_cod,cat_nombre,cat_descripcion,cat_estado) VALUES (1,'Inmuebles','Madera y de blanco',true)
 INSERT INTO UBICACIONES(ubi_cod,ubi_nombre, ubi_departamento, ubi_estado) values(1,'Maquinacion de Carpinteria','Lab-6',true);
-INSERT INTO BIENES (bien_cod, bien_codigoG, bien_modelo, bien_marca, bien_estado, bien_detalles, bien_descripcion, bien_serie, bien_propietario, bien_precio, bien_estadoA, bien_edi_cod,bien_cat_cod) VALUES (30778142, '###RSFDSF#####', '8120 0', 'FESTO', 'BUENO', 'MAQUINARIA Y EQUIPO', 'MAQUINARIA Y EQUIPO:1 BANCO MÓVIL PARA MÁQUINAS DESARMABLES 8120-0', 
-'', 'BENAVIDEZ PADILLA DARIO JAVIER', 0.0, true, 1,1);
+INSERT INTO BIENES (bien_cod, bien_codigoG, bien_modelo, bien_marca, bien_estado, bien_detalles, bien_descripcion, bien_serie, bien_propietario, bien_precio, bien_estadoA, bien_edi_cod,bien_cat_cod) VALUES (30778142, '###RSFDSF#####', '8120 0', 'FESTO', 'BUENO', 'MAQUINARIA Y EQUIPO', 'MAQUINARIA Y EQUIPO:1 BANCO MÓVIL PARA MÁQUINAS DESARMABLES 8120-0', '', 'BENAVIDEZ PADILLA DARIO JAVIER', 0.0, true, 1,1);
