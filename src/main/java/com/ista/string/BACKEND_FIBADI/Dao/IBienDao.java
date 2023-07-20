679e7bf82a584dc7561e934f2c8bfbb606635d96
Santiago G <santiagogcan@gmail.com>
2023-07-20 17:35:33 -0500

    New Fix

--- src/main/java/com/ista/string/BACKEND_FIBADI/Dao/IBienDao.java
+++ src/main/java/com/ista/string/BACKEND_FIBADI/Dao/IBienDao.java
@@ -23,6 +23,6 @@ public interface IBienDao extends CrudRepository<Bien, Long>{
 	
 	@Query(value = "SELECT * FROM BIENES b INNER JOIN UBICACIONES u ON (b.bien_edi_cod = u.ubi_cod) \r\n"
 			+ "	INNER JOIN CATEGORIA c ON (b.bien_cat_cod = c.cat_cod)\r\n "
-			+ "WHERE u.ubi_nombre LIKE :argument OR c.cat_nombre LIKE :argument", nativeQuery = true)
+			+ "WHERE u.ubi_nombre LIKE %:argument% OR c.cat_nombre LIKE %:argument%", nativeQuery = true)
 	List<Bien> findBienesByArgument(@Param("argument") String argument);
 }
