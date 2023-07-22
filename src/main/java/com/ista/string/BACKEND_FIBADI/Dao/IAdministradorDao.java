54f11b1afb4216bf6ff24e2d3b9d4d047ddc820c
Miguel13fdf <122501535+Miguel13fdf@users.noreply.github.com>
2023-07-22 10:47:07 -0500

    Revert "administrador"
    This reverts commit 81112d286698130bb53ef2ce5ee09bdfe31d085f.

--- src/main/java/com/ista/string/BACKEND_FIBADI/Dao/IAdministradorDao.java
+++
@@ -1,9 +0,0 @@
-package com.ista.string.BACKEND_FIBADI.Dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ista.string.BACKEND_FIBADI.Model.Administrador;
-
-public interface IAdministradorDao extends CrudRepository<Administrador, Long> {
-
-}
